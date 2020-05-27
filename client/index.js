2e453e1f505394ab36c80d74604b7053f7aa527c
Aatif Bandey <aatifbandey@Aatifs-Mac.local>
2020-05-27 22:17:37 +0530

    First commit -- test

--- client/index.js
+++ client/index.js
@@ -4,8 +4,6 @@
 // import App from "./App";
 
 
-// ReactDOM.render(<App />, document.querySelector("#root"));
-
 import React from 'react';
 import { render, hydrate } from 'react-dom';
 import Loadable from 'react-loadable';
